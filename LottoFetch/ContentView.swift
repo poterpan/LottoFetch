b6e5a354e0c8e68ed93ee7f4419b8c92fc2c95c2
PoterPan <poterpan5466@gmail.com>
2024-02-18 10:22:43 +0800

    Initial Commit

---
+++ LottoFetch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LottoFetch
+//
+//  Created by Poter Pan on 2024/2/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LottoFetch/LottoFetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottoFetchApp.swift
+//  LottoFetch
+//
+//  Created by Poter Pan on 2024/2/18.
+//
+
+import SwiftUI
+
+@main
+struct LottoFetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
